322a7cb8954c9d0ad26e1cc6bf623b2c011fa0f6
gabriel <gabrielsuarezcifuentes1221@gmail.com>
2025-11-13 22:34:36 -0500

    Error de syntaxis en los modelos corregido

--- src/models/Resena.js
+++ src/models/Resena.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const resenaSchema = new mongoose.Schema({
   juegoId: { type: mongoose.Schema.Types.ObjectId, ref: "Juego", required: true },
   puntuacion: { type: Number, min: 1, max: 5, required: true },
-  textoResena: { type: String, trim: true, requerid: true },
+  textoResena: { type: String, trim: true, required: true },
   horasJugadas: { type: Number, min: 0, required: true },
   dificultad: { type: String, enum: {
     values: ["Fácil", "Normal", "Difícil", "Muy Difícil"],
